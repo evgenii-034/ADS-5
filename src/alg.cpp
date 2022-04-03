9e78a11d3c902df87696b977b14b0cc3dd44f65e
evgenii-034 <99119985+evgenii-034@users.noreply.github.com>
2022-04-03 10:27:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,88 +1,29 @@
 // Copyright 2021 NNTU-CS
-#include <string>
-#include <map>
-#include "tstack.h"
-int pr(char op) {
-    switch (op) {
-      case '(': return 0;
-      case ')': return 1;
-      case '+': return 2;
-      case '-': return 2;
-      case '*': return 3;
-      case '/': return 3;
-      case ' ': return 4;
-      default: return 5; 
-    }
-  }
-
-int culcul(char op, int a, int b) {
-  switch (op) {
-    case '+': return b + a;
-    case '-': return b - a;
-    case '*': return b * a;
-    case '/': return b / a;
-    case '+': return a + b;
-    case '-': return a - b;
-    case '*': return a * b;
-    case '/': return a / b;
-    default: return 0;
-  }
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
+template<typename T, int size>
+class TStack {
+private:
+T arr[100];
+int top;
+public:
+TStack() :top(-1) { }
+T get() const {
+return arr[top];
 }
-std::string infx2pstfx(std::string inf) {
-  std::string res;
-  TStack <char, 100> sta;
-  for (int i = 0; i < inf.length(); i++) {
-    if (pr(inf[i]) == 5) {
-      res.push_back(inf[i]); 
-      res.push_back(' ');
-    } else {
-        if (pr(inf[i]) == 4) {
-          continue; 
-        } else if (pr(inf[i]) == 0) {
-          sta.push(inf[i]);
-        } else if (pr(inf[i]) > pr(sta.get())) {
-          sta.push(inf[i]);
-        } else if (sta.isEmpty()) {
-          sta.push(inf[i]);
-        } else if (pr(inf[i]) == 1) {
-          while (pr(sta.get()) != 0) { 
-            res.push_back(sta.get());
-            res.push_back(' ');
-            sta.pop(); 
-          }
-          sta.pop();
-        } else {
-            while (!sta.isEmpty() && (pr(inf[i]) <= pr(sta.get()))) {
-              res.push_back(sta.get());
-              res.push_back(' ');
-              sta.pop();
-            }
-            sta.push(inf[i]);
-        }
-     }
-  }
-  while (!sta.isEmpty()) {
-    res.push_back(sta.get());
-    res.push_back(' ');
-    sta.pop();
-  }
-  res.pop_back();
-  return res;
+bool isEmpty() const {
+return top == -1;
 }
-int eval(std::string pref) {
-  TStack <int, 100> stack2;
-  int res = 0, x, y;
-  for (int i = 0; i < pref.length(); i++) {
-    if (pr(pref[i]) == 5) {
-      stack2.push(pref[i]-'0');
-    } else if (pr(pref[i]) < 4) {
-      y = stack2.get();
-      stack2.pop();
-      x = stack2.get();
-      stack2.pop();
-      stack2.push(culcul(pref[i], x, y)); 
-    }
-  }
-  res = stack2.get();
-  return res;
+bool isFull() const {
+return top == size - 1;
 }
+void pop() {
+if (top >= 0)
+top--;
+}
+void push(T item) {
+if (top < size - 1)
+arr[++top] = item;
+}
+};
+#endif 
